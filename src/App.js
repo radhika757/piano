2b45f20eb846548a6038d4bd1a4fa8beccfbd4db
radhika <raoradhika2000@gmail.com>
2023-09-12 22:31:44 +0530

    boiler plate ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import 
+import './App.module.css';
+import { Piano } from './components/Piano';
+
+function App() {
+  return (
+    <div className="app">
+      <Piano/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Piano.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './Piano.module.css';
+
+export const Piano = () =>{
+    return(
+        <h2>I am piano</h2>
+    )
+}
