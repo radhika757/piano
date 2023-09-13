5247292c0e44dcafd52463962c101563ac6cda88
radhika <raoradhika2000@gmail.com>
2023-09-13 10:45:54 +0530

    build changes

--- src/components/VerticalBars.jsx
+++ src/components/VerticalBars.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './VerticalBars.module.css';
 
 export const VerticalBars = ({ height, onClick, barNumber }) => {
